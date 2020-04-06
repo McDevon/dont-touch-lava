2d1e47552eb1961465c0c0d0f79a3b1a70905bcd
McDevon <jussi.enroos@gmail.com>
2020-04-06 08:42:45 +0300

    Min update interval for ultrasonic sensor

--- logic.cpp
+++ logic.cpp
@@ -13,30 +13,41 @@
 
 CRGB leds[NUM_LEDS];
 
-void setup() {
+void setup()
+{
   delay(1000);
   Serial.begin(9600);
 
   pinMode(LED_BUILTIN, OUTPUT);
-  FastLED.addLeds<LED_TYPE,LED_DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, LED_DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(DEFAULT_BRIGHTNESS);
 
   initialize_us_sensor(US_PING_PIN, US_ECHO_PIN);
 }
 
 GameState game_state = CREATE_GAME_STATE(leds, NUM_LEDS);
+int delay_accumulated = INT16_MAX;
+long duration = 0;
+const int ultrasonic_min_interval = 50;
 
-void loop() {
-   long duration = activate_us_sensor(US_PING_PIN, US_ECHO_PIN);
-   long cm = us_microseconds_to_cm(duration);
-   Serial.print(cm);
-   Serial.print("cm, ");
-   Serial.print(duration);
-   Serial.print(" us");
-   Serial.println();
+void loop()
+{
+  if (delay_accumulated >= ultrasonic_min_interval) {
+    delay_accumulated = 0;
+    duration = activate_us_sensor(US_PING_PIN, US_ECHO_PIN);
 
-   int delay_time = step_game(&game_state, duration);
-   FastLED.show();
-   
-   delay(delay_time);
+    long cm = us_microseconds_to_cm(duration);
+    Serial.print(cm);
+    Serial.print("cm, ");
+    Serial.print(duration);
+    Serial.print(" us");
+    Serial.println();
+  } 
+
+  int delay_time = step_game(&game_state, duration);
+  delay_accumulated += delay_time;
+
+  FastLED.show();
+
+  delay(delay_time);
 }
